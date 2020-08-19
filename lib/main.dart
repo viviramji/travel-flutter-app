85cc4cb883e8d992c41f6d5408fb879ded00d66e
Victor Victor RJ <viviramji@gmail.com>
2020-08-19 14:41:39 -0500

    Gradient img

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:travel_app/description_place.dart';
 import 'review_list.dart';
+import 'gradient_back.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +9,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
+  int numberStars = 4;
+  String namePlace = "Turbaco";
+  String decriptionPlace =
+      "Lorem impsu Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,10 +24,15 @@ class MyApp extends StatelessWidget {
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         home: Scaffold(
-            appBar: AppBar(title: Text("Travel App")), body: ReviewList()
-
-            //DescriptionPlace(3, "Cartagena",
-            //   "Lorem impsu Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"),
-            ));
+            // appBar: AppBar(title: Text("Travel App")),
+            body: Stack(children: <Widget>[
+          ListView(
+            children: <Widget>[
+              DescriptionPlace(numberStars, namePlace, decriptionPlace),
+              ReviewList()
+            ],
+          ),
+          GradientBack()
+        ])));
   }
 }
